faa506a092c6b0b3f9525b237e1103b36c23cc74
dsigs <davidfsigno@live.com>
2024-06-14 19:39:56 -0500

    Edit javascript and main features

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ Input is only limited to one question that asks to pick between rock, paper, or 
 
 Output will display the results between a bot that randomizes the choices between r, p, s.
 
-# 
-#
+# function getComputerChoice 
+
+1. This function will randomize three choices and return them to the global 
+2. wont take input
+3. will return one of three inputs
+
--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
-console.log("hello, world")
+let playerChoice 
+
+do {
+    playerChoice = prompt("Pick between rock, paper, and scissors: ");
+}
+while (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') 
+
+console.log(playerChoice);
+
+function getComputerChoice() {
+     return Math.floor(Math.random() * 3)
+}
+
+let computerChoice = getComputerChoice();
+
+if (computerChoice == 0) {
+    console.log("Computer: rock");
+} else if (computerChoice == 1) {
+    console.log("Computer: paper");
+} else if (computerChoice == 2) {
+    console.log("Computer: scissors")
+}
+
+if (playerChoice == 'rock' && computerChoice == 2) {
+    console.log("You win!");
+} else if (playerChoice == 'rock' && computerChoice == 1) {
+    console.log("You lose");
+} else {
+    console.log("Tie");
+}
