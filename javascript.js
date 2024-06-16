3bf189317f6094bc8285e1eac4cb949621ec8836
dsigs <davidfsigno@live.com>
2024-06-16 14:48:44 -0500

    Refactor playRound and finish playGame function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+let playerSelection 
+let computerSelection
 let playerScore = 0;
 let computerScore = 0;
-let result ='Game';
 
 function getPlayerChoice(playerSelection) {
     do {
@@ -29,34 +28,37 @@ function getComputerChoice(computerSelection) {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
         playerScore++;
-        return console.log("You win");
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return console.log("Round won");
+    } else if (playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'rock') {
         computerScore++;
-        return console.log("You lose");
-    }  else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore++;
-        return console.log("You win");
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore++
-        return console.log("You lose");
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore++;
-        return console.log("You win");
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore++;
-        return console.log("You lose");
+        return console.log("Round lost");
     } else {
-        console.log("Tie");
+        return console.log("Round tie");
     }
 }
 
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        playerSelection = getPlayerChoice();        
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log('You picked:', playerSelection);
+        console.log('You picked:', computerSelection);
+        console.log('\n')
+    }  
 
+    console.log(playerScore);
+    console.log(computerScore);
+    if (playerScore > computerScore) {
+        console.log('You win!') ;
+    } else if (playerScore < computerScore) {
+        console.log('You lose.');
+    } else {
+        console.log('Game tie!');
+    }
+}
 
-playRound(playerSelection, computerSelection);
+playGame();
 
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playerScore);
-console.log(computerScore);
